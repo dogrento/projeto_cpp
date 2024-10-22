45f7a21b75abb8364346eb366c3b330e1a6ae023
dogrento <dogrento93@gmail.com>
2024-10-22 20:32:29 -0300

    criando classe Professor que herda de Pessoa

--- Professor.h
+++ Professor.h
@@ -1,37 +1,30 @@
-#ifndef PESSOA_H
-#define PESSOA_H
+// Classe Professor herda de Pessoa
+#pragma onde
 
 #include <string.h>
 #include "universidade.h"
 #include "dpto.h"
+#include "pessoa.h"
 
-class Pessoa{
-private:
-  int diaP;
-  int mesP;
-  int anoP;
-  int idadeP;
-  char nomeP[30];
-  // Universidade* uniP; // referencia a um objeto associado (Universidade no caso)
-  // Dpto* dptoP; // referencia a um objeto associado (Dpto no caso)
+class Professor:public Pessoa{
+protected: // protected permite herança
+
+  Universidade* uniP; // referencia a um objeto associado (Universidade no caso)
+  Dpto* dptoP; // referencia a um objeto associado (Dpto no caso)
 
 public:
   // Construtora com valor default
   // NOTA* nao é possivel definir valor default caso os parametros mais a direita nao estejam com valor default. 
   // Ou seja, colcar os paremetro com default value sempre a direita.
   // Valores default sempre no .h e nao no .cpp
-  Pessoa(int dia, int mes, int ano, const char nome[] = "");
-  Pessoa(); // Toda classe deve ter construtora sem param.
-  ~Pessoa();
+  Professor(int dia, int mes, int ano, const char nome[] = "");
+  Professor(); // Toda classe deve ter construtora sem param.
+  ~Professor();
 
   // Metodos
   void init(int dia, int mes, int ano, const char nome[] = "");
-  void calcIdade(int diaAtual, int mesAtual, int anoAtual);
-  int getIdade();
   void setUniversidade(Universidade* uni);
   void setDpto(Dpto* d);
   void informaUni();
   void informaInfo();
 };
-
-#endif
