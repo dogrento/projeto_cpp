1f1608d8b8084025f059ae384a3830eb5bcd18af
dogrento <dogrento93@gmail.com>
2024-10-23 13:21:55 -0300

    importando iostream

--- aluno.h
+++ aluno.h
@@ -1,9 +1,15 @@
 #pragma once
 
+#include <iostream>
+using namespace std;
+
+#include "pessoa.h"
 #include "universidade.h"
 #include "dpto.h"
 
-class Aluno{
+class Aluno:public Pessoa{ // Aluno herda de Pessoa
+private:
+  char ra[7];  
 protected:// protected permite herança
   Universidade* uniP; // referencia a um objeto associado (Universidade no caso)
   Dpto* dptoP; // referencia a um objeto associado (Dpto no caso)
@@ -13,7 +19,7 @@ public:
   // NOTA* nao é possivel definir valor default caso os parametros mais a direita nao estejam com valor default. 
   // Ou seja, colcar os paremetro com default value sempre a direita.
   // Valores default sempre no .h e nao no .cpp
-  Aluno(int dia, int mes, int ano, const char nome[] = "");
+  Aluno(int dia, int mes, int ano, const char nome[] = "", const char ra[]="");
   Aluno(); // Toda classe deve ter construtora sem param.
   ~Aluno();
 
